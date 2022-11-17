4c010abce392e7bec7f87424ece72cb66e58ed21
Neo Liao <neo@Neos-MacBook-Pro.local>
2022-11-17 23:26:28 +0800

    mongo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # java-middlewares
 一些java的常用中间件或者第三方包
+
+
+## github project
+https://github.com/spring-projects/spring-data-book/tree/master/mongodb
+
+https://github.com/mongodb-developer/mongodb-springboot/blob/main/mdb-spring-boot/pom.xml
+
+https://www.mongodb.com/compatibility/spring-boot
---
+++ mongodb/src/main/java/org/example/mongodb/model/GroceryItem.java
@@ -0,0 +1,24 @@
+package org.example.mongodb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("GroceryItem")
+@Getter
+@Setter
+@AllArgsConstructor
+public class GroceryItem {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private int quantity;
+
+    private String category;
+}
