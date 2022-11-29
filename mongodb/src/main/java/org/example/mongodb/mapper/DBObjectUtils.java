70986981d32776318628253b92b4b81f4a946093
neo.liao <ljpfuture@gmail.com>
2022-11-30 00:28:56 +0800

    DBObject 和 pojo 的转化, 基于注解 改成用stream

--- mongodb/src/main/java/org/example/mongodb/mapper/DBObjectUtils.java
+++ mongodb/src/main/java/org/example/mongodb/mapper/DBObjectUtils.java
@@ -4,8 +4,8 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.example.mongodb.model.Column;
 
-import javax.naming.OperationNotSupportedException;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 
 public class DBObjectUtils {
@@ -14,63 +14,44 @@ public class DBObjectUtils {
     public static <T> T toTojo(DBObject dbObject, Class<T> clazz) throws InstantiationException, IllegalAccessException {
         Field[] declaredFields = clazz.getDeclaredFields();
         T t = clazz.newInstance();
-        for (Field declaredField : declaredFields) {
-            if (!declaredField.isAnnotationPresent(Column.class)) {
-                continue;
-            }
-
-            declaredField.setAccessible(true);
-            Column annotation = declaredField.getAnnotation(Column.class);
-            String key = annotation.value().equals("") ? declaredField.getName() : annotation.value();
-            if (!dbObject.containsField(key)) {
-                continue;
-            }
-
-            declaredField.set(t, dbObject.get(key));
-        }
+        Arrays.stream(declaredFields)
+                .filter(field -> field.isAnnotationPresent(Column.class))
+                .forEach(field -> {
+                    field.setAccessible(true);
+                    Column annotation = field.getAnnotation(Column.class);
+                    String key = annotation.value().equals("") ? field.getName() : annotation.value();
+                    if (dbObject.containsField(key)) {
+                        try {
+                            field.set(t, dbObject.get(key));
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
         return t;
     }
 
-    public static <T> DBObject toDBObject(T pojo) throws IllegalAccessException {
+    public static <T> DBObject toDBObject(T pojo) {
         if (pojo == null) {
             return new BasicDBObject();
         }
-        Field[] declaredFields = pojo.getClass().getDeclaredFields();
+
         BasicDBObject basicDBObject = new BasicDBObject();
-        for (Field declaredField : declaredFields) {
-            if (!declaredField.isAnnotationPresent(Column.class)) {
-                continue;
-            }
+        Arrays.stream(pojo.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Column.class))
+                .forEach(field -> {
+                    field.setAccessible(true);
+                    Column annotation = field.getAnnotation(Column.class);
+                    String key = annotation.value().equals("") ? field.getName() : annotation.value();
+                    Object value;
+                    try {
+                        value = field.get(pojo);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                    basicDBObject.put(key, value);
 
-            Column annotation = declaredField.getAnnotation(Column.class);
-            declaredField.setAccessible(true);
-            String key = annotation.value().equals("") ? declaredField.getName() : annotation.value();
-
-            Object o = declaredField.get(pojo);
-            // 没有限制 null
-            if (o == null) {
-                basicDBObject.put(key, null);
-                continue;
-            }
-//            Class<?> type = declaredField.getType();
-            basicDBObject.put(key, o);
-//            if (type.isPrimitive()) {
-//                basicDBObject.put(key, o);
-//            } else if (type.equals(Integer.class) || type.equals(String.class)
-//                    || type.equals(Boolean.class) || type.equals(Long.class)) {
-//                basicDBObject.put(key, o);
-//            } else if (type.equals(Map.class)) {
-//                basicDBObject.put(key, o);
-//            } else if (type.equals(List.class)) {
-//                basicDBObject.put(key, o);
-//            } else if (type.equals(Date.class)) {
-//                basicDBObject.put(key, o);
-//
-//            } else {
-//                throw new OperationNotSupportedException("暂不支持转换 " + type.getName() + " 类型的字段");
-//            }
-
-        }
+                });
         return basicDBObject;
     }
 }
